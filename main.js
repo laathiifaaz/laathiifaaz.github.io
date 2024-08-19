9c741f5e1d22000cc0cd0d665c97b78ac2d36431
laathiifaaz <zahiralathiifa@apps.ipb.ac.id>
2024-08-19 08:44:19 +0700

    main.js

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 const toTop = document.querySelector(".to-top");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollTo > 100) {
-        toTop.classList.add("active");
-    } else {
-        toTop.classList.remove("active");
-    }
-});
-
-toTop.addEventListener("click", (e) => {
-    e.preventDefault();
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
